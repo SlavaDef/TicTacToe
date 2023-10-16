e51c5190285b84d844b32b80ac9b34572121508a
SlavaDef <slavadef@gmail.com>
2023-10-16 19:28:38 +0300

    second commit

--- src/main/java/com/example/tictactoe2/HelloController.java
+++ src/main/java/com/example/tictactoe2/HelloController.java
@@ -2,9 +2,13 @@ package com.example.tictactoe2;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
 
@@ -14,18 +18,81 @@ public class HelloController {
     @FXML
     private URL location;
 
-   private char nowSymb = 'X';
+    private char nowSymb = 'X';
+
+    private boolean isGame = true;
+
+    private char gameField[][] = new char[3][3]; // ігрове поле
 
     @FXML
     void btClick(ActionEvent event) { // логіка кнопки, event обьект з яким працюємо
-        ((Button)event.getSource()).setText(String.valueOf(nowSymb));
-        // логіка по зміні X i 0 кожне нажатіе новий символ
+
+        Button btn = ((Button) event.getSource()); // кнопка з якою працюємо
+
+        if (!isGame || btn.getText() != "") return; // isGame == false - вихід з методу
+
+        // шукаємо індекс ряду + обробка нуля
+        int rowIndex = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
+        // індекс колонки + обробка нуля
+        int columIndex = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
+
+        gameField[rowIndex][columIndex] = nowSymb;
+
+
+        btn.setText(String.valueOf(nowSymb));
+
+        // логіка пошуку переможця
+        // якщо перша ячейка рівна другій і перша рівна третій і перша ячейка не порожня
+        if (gameField[0][0] == gameField[0][1] && gameField[0][0] == gameField[0][2]
+                && (gameField[0][0] == 'X' || gameField[0][0] == '0')) {
+            messageShowing(event);
+            isGame = false;
+        } else if (gameField[1][0] == gameField[1][1] && gameField[1][0] == gameField[1][2]
+                && (gameField[1][0] == 'X' || gameField[1][0] == '0')) {
+            messageShowing(event);
+            isGame = false;
+        } else if (gameField[2][0] == gameField[2][1] && gameField[2][0] == gameField[2][2]
+                && (gameField[2][0] == 'X' || gameField[2][0] == '0')) {
+            messageShowing(event);
+            isGame = false;
+        } else if (gameField[0][0] == gameField[1][0] && gameField[0][0] == gameField[2][0]
+                && (gameField[0][0] == 'X' || gameField[0][0] == '0')) {
+            messageShowing(event);
+            isGame = false;
+        } else if (gameField[0][1] == gameField[1][1] && gameField[0][1] == gameField[2][1]
+                && (gameField[0][1] == 'X' || gameField[0][1] == '0')) {
+            messageShowing(event);
+            isGame = false;
+        } else if (gameField[0][2] == gameField[1][2] && gameField[0][2] == gameField[2][2]
+                && (gameField[0][2] == 'X' || gameField[0][2] == '0')) {
+            messageShowing(event);
+            isGame = false;
+        } else if (gameField[0][0] == gameField[1][1] && gameField[0][0] == gameField[2][2]
+                && (gameField[0][0] == 'X' || gameField[0][0] == '0')) {
+            messageShowing(event);
+            isGame = false;
+        } else if (gameField[0][2] == gameField[1][1] && gameField[0][2] == gameField[2][0]
+                && (gameField[0][2] == 'X' || gameField[0][2] == '0')) {
+            messageShowing(event);
+            isGame = false;
+        }
+
+
+        // логіка по зміні X i 0 кожне натиснення -  новий символ
         nowSymb = nowSymb == 'X' ? '0' : 'X';
+    }
+
+    // метод для виведення сповіщення про переможця
+    public Alert messageShowing(ActionEvent event) {
+        Button btn = ((Button) event.getSource());
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have the  winner, congratulations! - " + btn.getText(), ButtonType.OK);
+        alert.showAndWait();
+        return alert;
     }
 
     @FXML
     void initialize() {
-      // стандартна функція накшталт PostConstruct
+        // стандартна функція накшталт PostConstruct
     }
 
 }
